541dbf2546646ebed3ad4e0f871a64284058ae60
Duc Filan <ducfilan@gmail.com>
2019-08-17 17:23:41 +0900

    Extension code

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,41 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as pugBeautify from 'pug-beautify';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	vscode.languages.registerDocumentFormattingEditProvider('jade', {
+		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+			let tabSize = 4;
+			let insertSpaces = true;
+			const editor = vscode.window.activeTextEditor;
+			if (editor) {
+				tabSize = editor.options.tabSize as number;
+				insertSpaces = editor.options.insertSpaces as boolean;
+			}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-pug-formatter" is now active!');
+			const editorConfig = <any>vscode.workspace.getConfiguration('pugFormatter');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+			const text = document.getText();
+			const options = {
+				fill_tab: editorConfig.fillTab || insertSpaces,
+				omit_div: editorConfig.omitDiv,
+				tab_size: editorConfig.tabSize || tabSize
+			};
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+			let result = '';
+			try {
+				result = pugBeautify(text, options);
+			} catch (err) {
+				vscode.window.showErrorMessage(err);
+			}
+
+			const lastLine = document.lineAt(document.lineCount - 1);
+			const start = new vscode.Position(0, 0);
+			const end = new vscode.Position(document.lineCount - 1, lastLine.text.length);
+			const range = new vscode.Range(start, end);
+
+			return [vscode.TextEdit.replace(range, result)];
+		}
 	});
-
-	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
